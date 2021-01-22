cd195f1bc95f4814515694b7754a7bc81845b14c
Ironfoot267 <17611556-075@uogsialkot.edu.pk>
2021-01-22 23:07:05 +0500

    Index Activity

---
+++ app/src/main/java/com/example/s_f_login/Indexfile.java
@@ -0,0 +1,14 @@
+package com.example.s_f_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Indexfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indexfile);
+    }
+}
